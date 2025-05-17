46fdd8c0d3db418669c18d69bb7548d379a1c41e
Jonathan Cantora <tantancantora@gmail.com>
2025-05-17 15:29:36 +0800

    chore

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "Generating config.env..."
 
 cat <<EOF > ./config.env
-DATABASE_URL=${DATABASE_URL}
-PORT=${PORT:-8080}
-SECRET_KEY=${SECRET_KEY}
+DATABASE_URL=mongodb://root:rvCxs4Q1483XyS47Zl6l642yBKkGUDWXvwlluSAefIIVjbRdinhjM1uFHrelnMTW@xw0w8okc0c44c0sc4sgk48ok:27017/?directConnection=true
+PORT=8080
+SECRET_KEY=xw0w8okc0c44c
 EOF
 
 echo "Config ENV created"
